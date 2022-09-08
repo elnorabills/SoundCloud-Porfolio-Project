12dedac69f802a5eafb605cc36854b7a2f77f7a0
Eleanor Billerbeck <ellie.billerbeck@gmail.com>
2022-09-07 20:17:26 -0500

    All Songsgit status

--- frontend/src/components/AllSongs/allSongs.js
+++ frontend/src/components/AllSongs/allSongs.js
@@ -4,27 +4,28 @@ import { useSelector, useDispatch } from "react-redux";
 import "./AllSongs.css";
 
 function AllSongs() {
-    const dispatch = useDispatch();
-    const songs = useSelector(state => state.songs);
+  const dispatch = useDispatch();
+  const songs = useSelector((state) => state.songs);
 
-    useEffect(() => {
-        dispatch(allSongsThunk())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(allSongsThunk());
+  }, [dispatch]);
 
-    return (
-      <div className="all-songs-large-container">
-        <h1 className="h1-all-songs-title">Songs</h1>
-        <div className="all-songs-container">
-          {Object.keys(songs).map((songId) => {
-            return (
-              <div className="all-songs" id={songId}>
-                {songs[songId].title}
-              </div>
-            );
-          })}
-        </div>
+  return (
+    <div className="all-songs-large-container">
+      <h1 className="h1-all-songs-title">Songs</h1>
+      <div className="all-songs-container">
+        {Object.keys(songs).map((songId) => {
+          return (
+            <div className="all-songs" id={songId}>
+              {songs[songId].title}
+            </div>
+          );
+        })}
       </div>
-    );
+    </div>
+  );
 }
 
 export default AllSongs;
+
