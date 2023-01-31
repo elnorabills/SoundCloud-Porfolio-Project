ca0bf673214a744ed6ac471ff24884ca2ab2e64c
Eleanor Billerbeck <ellie.billerbeck@gmail.com>
2023-01-30 19:08:24 -0600

    splash page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,21 +2,25 @@ import "./Home.css";
 import { NavLink } from "react-router-dom";
 
 function HomePage () {
-
-    return (
-      <div className="home-page-container">
-        <h1 className="signup-home-page-title">Sign Up Now</h1>
-        <h2 className="sign-up-home-page-description">
-          Upload your first track and begin your journey. SoundCloud gives you
-          space to create, find your fans, and connect with other artists.
-        </h2>
-        <div className="signup-nav-link-container">
-          <NavLink className="nav-link-sign-up" to="/sign-up">
-            Sign Up
-          </NavLink>
-        </div>
-      </div>
-    );
+  return (
+    <div className="home-page-container">
+      
+    </div>
+  )
+    // return (
+    //   <div className="home-page-container">
+    //     <h1 className="signup-home-page-title">Sign Up Now</h1>
+    //     <h2 className="sign-up-home-page-description">
+    //       Upload your first track and begin your journey. SoundCloud gives you
+    //       space to create, find your fans, and connect with other artists.
+    //     </h2>
+    //     <div className="signup-nav-link-container">
+    //       <NavLink className="nav-link-sign-up" to="/sign-up">
+    //         Sign Up
+    //       </NavLink>
+    //     </div>
+    //   </div>
+    // );
 }
 
 export default HomePage;
